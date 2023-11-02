b5e996449207c2f58627380b009854ae2f38971f
Usama Ahtsham <usamaaht@gmail.com>
2023-11-02 12:58:47 +0500

    initial commit

---
+++ Olufeson/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from turn.views import *
+from rest_framework_simplejwt.views import(
+    TokenObtainPairView,
+    TokenRefreshView
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home', home, name='home'),
+    path('post-todo', post_todo, name='post-todo'),
+    path('get-todo' , get_todo, name='get-todo'),
+    path('delete', delete_todo, name='delete'),
+    path('update_patch', update_patch, name='update_patch'),
+    path('class', TodoClass.as_view(), name='class')
+] 
